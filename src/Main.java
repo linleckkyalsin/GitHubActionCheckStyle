bd54e795921c75989bffb199545952d980a355f3
linleckkyalsin <linleckkyalsin@gmail.com>
2022-08-15 12:20:11 +0630

    create method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        displayName("Lin Leck");
+    }
+    public static   void displayName(String name){
+        System.out.println("Hello"+name);
     }
 }
